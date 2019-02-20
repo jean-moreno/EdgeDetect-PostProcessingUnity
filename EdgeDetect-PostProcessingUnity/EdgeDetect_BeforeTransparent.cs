f5f6033f0a6662566dd863fb779088a93066d21b
Jean Moreno <jean.moreno.fr@gmail.com>
2019-02-20 07:37:20 +0100

    fixed "Before Transparent" effect

--- EdgeDetect-PostProcessingUnity/EdgeDetect_BeforeTransparent.cs
+++ EdgeDetect-PostProcessingUnity/EdgeDetect_BeforeTransparent.cs
@@ -10,9 +10,9 @@ using UnityEngine.Rendering.PostProcessing;
 //--------------------------------------------------------------------------------------------------------------------------------
 
 [System.Serializable]
-[PostProcess(typeof(EdgeDetectPostProcessingRenderer_BeforeStack), PostProcessEvent.BeforeTransparent, "Unity Legacy/Edge Detection (Before Transparent)")]
+[PostProcess(typeof(EdgeDetectPostProcessingRenderer_BeforeTransparent), PostProcessEvent.BeforeTransparent, "Unity Legacy/Edge Detection (Before Transparent)")]
 public sealed class EdgeDetect_BeforeTransparent : EdgeDetectPostProcessing { }
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
-public sealed class EdgeDetectPostProcessingRenderer_BeforeTransparent : EdgeDetectPostProcessingRenderer<EdgeDetect_BeforeStack> { }
+public sealed class EdgeDetectPostProcessingRenderer_BeforeTransparent : EdgeDetectPostProcessingRenderer<EdgeDetect_BeforeTransparent> { }
